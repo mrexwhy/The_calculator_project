f4798950e57b381b54935075066016db75cbf584
mrexwhy <153110286+mrexwhy@users.noreply.github.com>
2024-06-08 10:23:52 +0530

    Added the basic math functions

---
+++ script.js
@@ -0,0 +1,15 @@
+const addition = function (a, b) {
+    return a + b;
+};
+
+const substract = function (a, b) {
+    return a - b;
+};
+
+const multiply = function (array) {
+    return array.reduce((total, current) => total *= current);
+};
+
+const division = function (a, b) {
+    return a / b;
+};
