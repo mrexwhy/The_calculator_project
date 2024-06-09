81ce95bbc48bb9dd73cb8c7f6614d745cb2ba42e
mrexwhy <153110286+mrexwhy@users.noreply.github.com>
2024-06-09 10:57:28 +0530

    Add

--- script.js
+++ script.js
@@ -6,10 +6,35 @@ const substract = function (a, b) {
     return a - b;
 };
 
-const multiply = function (array) {
-    return array.reduce((total, current) => total *= current);
+const multiply = function (a, b) {
+    return a * b;
 };
 
 const division = function (a, b) {
     return a / b;
 };
+
+let firstNumber;
+let operator;
+let secondNumber;
+
+function operate (num1, op, num2) {
+    let result;
+    switch(op) {
+        case '+':
+            result = addition(num1, num2);
+            break;
+        case '-':
+            result = substract(num1, num2);
+            break;
+        case '*':
+            result = multiply(num1, num2);
+            break;
+        case '/':
+            result = division(num1, num2);
+            break;             
+        default:
+            result = 'Invalid Operation';    
+    }
+    return result;
+}
